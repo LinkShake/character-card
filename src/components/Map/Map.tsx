24eb91067fe93c17a0bbae3c6a71cc5287134b92
LinkShake <nicolacampo19@gmail.com>
2023-11-04 08:04:35 +0100

    Fixed zoom behavior on map component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -11,7 +11,7 @@ export default function Map() {
   const zoom = 13;
 
   return (
-    <MapContainer center={position as any} zoom={zoom} scrollWheelZoom={true}>
+    <MapContainer center={position as any} zoom={zoom} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
